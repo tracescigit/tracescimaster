ce7569229e72890e6b84921ed9c86846c7e05d5c
Shivam <tr.ui@tracesci.in>
2026-06-02 17:54:36 +0530

    changes

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -41,7 +41,7 @@
                 <img width="20%" alt="Tracesci" src="{{asset('web/images/logo.png')}}" class=""></a>
             </a>
             <div class="my-auto">
-                <img alt="Midone Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/illustration.svg') }}">
+                <img alt="Midone Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/illustration.png') }}">
                 <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">Just one step to <br> create your account.</div>
                 <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-gray-500">Create your account with us.</div>
             </div>
